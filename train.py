ee25aff945777621ed8daf36f0b322c16241da50
67htb5vn <maivunguyetanh@gmail.com>
2026-07-30 10:30:54 +0700

    data

--- train.py
+++ train.py
@@ -19,8 +19,8 @@ from unet import UNet
 from utils.data_loading import BasicDataset, CarvanaDataset
 from utils.dice_score import dice_loss
 
-dir_img = Path('./data/imgs/')
-dir_mask = Path('./data/masks/')
+dir_img = Path('/kaggle/input/datasets/nguyetanhmai/unet-train-medical/train_unet/images')
+dir_mask = Path('/kaggle/input/datasets/nguyetanhmai/unet-train-medical/train_unet/masks')
 dir_checkpoint = Path('./checkpoints/')
 
 
